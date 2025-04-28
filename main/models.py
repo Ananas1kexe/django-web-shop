81a773f43b15598b128185932443fc06589a3da7
ananas1k <sashafarberov2010@gmail.com>
2025-04-28 21:26:22 +0300

    Add files via upload

--- main/models.py
+++ main/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField("Name", max_length=50)
     description = models.CharField("Description", max_length=500)
+    image = models.ImageField("Image")
+    price = models.IntegerField("Price", default=0, validators=[MinValueValidator(0)])
     
     def __str__(self):
         return self.title
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django_ratelimit.decorators import ratelimit
-from .models import Books
+from .models import Book
 from .models import User
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.hashers import make_password, check_password
@@ -11,7 +11,7 @@ def index(request):
 
     user_id = request.session.get("user_id")
     user = User.objects.get(id=user_id) if user_id else None
-    books = Books.objects.all()
+    books = Book.objects.all()
     return render(request, "index.html", {"books": books, "user": user})
     
     
@@ -29,8 +29,21 @@ def register(request):
 
         new_user = User(username=username, password=hashed_password)
         new_user.save()
-        return redirect("login")
-    
+        
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = User.objects.filter(username=username).first()
+            
+        if  user and check_password(password, user.password):
+            auth_login(request, user)
+            request.session["user_id"] = user.id
+            return redirect("index")
+
+        else:
+            return render(request, "login.html", {"error": "Not corect username or password"})    
+        
     return render(request, "register.html")
 
 @ratelimit(key="ip", rate="10/m", method=["POST", "GET"], block=True)
@@ -39,17 +52,16 @@ def login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        try:
-            user = User.objects.filter(username=username).first()
-            
-            if check_password(password, user.password):
-                auth_login(request, user)
-                request.session["user_id"] = user.id
-                return redirect("index")
-
-            else:
-                return render(request, "login.html", {"error": "Not corect username or password"})
         
-        except User.DoesNotExist:
-                return render(request, "login.html", {"error": "not corect username or password"})
+        user = User.objects.filter(username=username).first()
+            
+        if  user and check_password(password, user.password):
+            auth_login(request, user)
+            request.session["user_id"] = user.id
+            return redirect("index")
+
+        else:
+            return render(request, "login.html", {"error": "Not corect username or password"})
+        
+
     return render(request, "login.html")
